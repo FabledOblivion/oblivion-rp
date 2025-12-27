8f393a02d9f8cd0230e6238213c0352c80b51f23
FabledOblivion <leontyev.vladimir1@gmail.com>
2025-12-27 11:15:12 -0800

    Characters MVP: endpoints + JSON sheet + basic UI + CI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Chat from './pages/Chat';
 import Placeholder from './pages/Placeholder';
+import CharacterList from './pages/CharacterList';
+import CharacterSheet from './pages/CharacterSheet';
 
 function App() {
   return (
@@ -12,9 +14,10 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/campaign/:id/chat" element={<Chat />} />
       <Route path="/campaign/:id/map" element={<Placeholder name="Map" />} />
-      <Route path="/campaign/:id/character" element={<Placeholder name="Character" />} />
+    <Route path="/campaign/:id/character" element={<CharacterList />} />
       <Route path="/ooc" element={<Placeholder name="OOC" />} />
       <Route path="/campaign/:id/settings" element={<Placeholder name="Settings" />} />
+            <Route path="/characters/:characterId" element={<CharacterSheet />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
